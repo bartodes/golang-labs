ce69c101bc7836e1ffa65d9d0f73b7f6e1038f1b
bartodes <bartojdes@gmail.com>
2023-04-27 11:14:42 -0300

    feat: added loop and functional approach

--- 2-playlist/main.go
+++ 2-playlist/main.go
@@ -7,34 +7,55 @@ import (
 	"strings"
 )
 
+var scanner = bufio.NewScanner(os.Stdin)
+
 func main() {
 	var playlist_name string
 	var song string
 	var playlist []string
-	scanner := bufio.NewScanner(os.Stdin)
 
+	readPlaylistName(&playlist_name)
+
+	exit := playlist_name == "exit"
+
+	for !exit {
+		fmt.Printf("Choose a song for '%s' playlist: ", playlist_name)
+		readSongName(&song)
+
+		if song == "exit" {
+			break
+		}
+
+		valid_playlist := len(strings.TrimSpace(playlist_name)) > 0
+		valid_song := len(strings.TrimSpace(song)) > 0
+
+		if !valid_playlist {
+			fmt.Println("Playlist name mustn't be nothing.")
+			return
+		}
+
+		if !valid_song {
+			fmt.Println("Song name mustn't be nothing.")
+			return
+		}
+
+		playlist = append(playlist, song)
+	}
+
+	fmt.Printf("Here are all the songs in '%s' playlist: %q Enjoy!\n", playlist_name, playlist)
+	fmt.Println("Bye bye!")
+}
+
+func readPlaylistName(name *string) {
+
+	fmt.Print("Type 'exit' when you are done!\n\n")
 	fmt.Print("Choose your playlist name: ")
 	scanner.Scan()
-	playlist_name = scanner.Text()
 
-	fmt.Print("Choose your song: ")
+	*name = scanner.Text()
+}
+
+func readSongName(name *string) {
 	scanner.Scan()
-	song = scanner.Text()
-
-	valid_playlist := len(strings.TrimSpace(playlist_name)) > 0
-	valid_song := len(strings.TrimSpace(song)) > 0
-
-	if !valid_playlist {
-		fmt.Println("Playlist name mustn't be nothing.")
-		return
-	}
-
-	if !valid_song {
-		fmt.Println("song name mustn't be nothing.")
-		return
-	}
-
-	playlist = append(playlist, song)
-	fmt.Printf("%#v has been added to the playlist '%v'\n", playlist, playlist_name)
-
+	*name = scanner.Text()
 }
