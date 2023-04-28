4b8fb279c325e4284e780acb66d992e34ea86866
bartodes <bartojdes@gmail.com>
2023-04-28 08:11:18 -0300

    feat: unified repetetive function and cleaner code

--- 2-playlist/main.go
+++ 2-playlist/main.go
@@ -7,20 +7,22 @@ import (
 	"strings"
 )
 
-var scanner = bufio.NewScanner(os.Stdin)
-
 func main() {
-	var playlist_name string
-	var song string
-	var playlist []string
+	var (
+		playlist_name string
+		song          string
+		playlist      []string
+	)
 
-	readPlaylistName(&playlist_name)
+	fmt.Print("Type 'exit' when you are done!\n\n")
+	fmt.Print("Choose your playlist name: ")
+	readName(&playlist_name)
 
 	exit := playlist_name == "exit"
 
 	for !exit {
 		fmt.Printf("Choose a song for '%s' playlist: ", playlist_name)
-		readSongName(&song)
+		readName(&song)
 
 		if song == "exit" {
 			break
@@ -42,20 +44,12 @@ func main() {
 		playlist = append(playlist, song)
 	}
 
-	fmt.Printf("Here are all the songs in '%s' playlist: %q Enjoy!\n", playlist_name, playlist)
-	fmt.Println("Bye bye!")
+	fmt.Printf("Here are all the songs in '%s': %q\n", playlist_name, playlist)
+	fmt.Println("Enjoy! Bye")
 }
 
-func readPlaylistName(name *string) {
-
-	fmt.Print("Type 'exit' when you are done!\n\n")
-	fmt.Print("Choose your playlist name: ")
-	scanner.Scan()
-
-	*name = scanner.Text()
-}
-
-func readSongName(name *string) {
+func readName(name *string) {
+	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
 	*name = scanner.Text()
 }
